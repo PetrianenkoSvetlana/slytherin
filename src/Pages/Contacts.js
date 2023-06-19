7439e174059dcfcc97f6c52d5d466f796151e568
PetrianenkoSvetlana <swetlana.petrianenko@yandex.ru>
2023-06-20 01:57:26 +0300

    Add Contact us

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
+import {Button, Container, Form} from "react-bootstrap";
 
 class Contacts extends Component {
     render() {
         return (
-            <div>
-                Contacts
-            </div>
+            <Container style={{width: '500px'}}>
+                <h1 className="text-center">Contact us</h1>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email"/>
+                        <Form.Text>
+                            Yes, we are now on the world Wide web. And no, the Acromantulas didn't take over the world. We just become open to the world. But we won't reveal your secrets.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control as="textarea" rows="3"/>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicCheckBox">
+                        <Form.Check type="checkbox" label="Check me out"/>
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">Submit</Button>
+                </Form>
+            </Container>
         );
     }
 }
