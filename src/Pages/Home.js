4d33551a12a5c62e44df2cae5755d014425f822b
PetrianenkoSvetlana <swetlana.petrianenko@yandex.ru>
2023-06-21 17:18:46 +0300

    Add cards to the Home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,84 @@
 import React, {Component} from 'react';
 import CarouselBox from "../Components/CarouselBox";
+import {Card, CardGroup, Container} from "react-bootstrap";
+import Art from "../Assets/Art.jpg";
+import cuteSnake from "../Assets/Cute_Snake.jpg";
+import girl from "../Assets/Girl_from_Slytherin.jpg";
+import sortingHat from "../Assets/Sorting_Hat.jpg";
 
 class Home extends Component {
     render() {
         return (
-            <CarouselBox/>
+            <>
+                <CarouselBox/>
+                <Container className="mb-4">
+                    <h2 className="text-center m-4">Something interesting</h2>
+                    <CardGroup>
+                        <div className="row row-cols-md-4">
+                            <div className="col mt-4">
+                                <Card className="h-100" border="light">
+                                    <Card.Img
+                                        variant="top"
+                                        src={Art}
+                                    />
+                                    <Card.Body>
+                                        <Card.Title>Slytherin Art</Card.Title>
+                                        <Card.Text>Slytherin has become the most popular faculty in Hogwarts Legacy — based on Achievement statistics.</Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+
+                            <div className="col mt-4">
+                                <Card className="h-100" border="light">
+                                    <Card.Img
+                                        variant="top"
+                                        src={cuteSnake}
+                                    />
+                                    <Card.Body>
+                                        <Card.Title>Cute Snake</Card.Title>
+                                        <Card.Text>Hermione Granger: "Sir — what exactly do you mean by the ‘horror within’ the Chamber?"</Card.Text>
+                                        <Card.Text>Professor Binns: "That is believed to be some sort of monster, which the Heir of Slytherin alone can control."</Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+
+                            <div className="col mt-4">
+                                <Card className="h-100" border="light">
+                                    <Card.Img
+                                        variant="top"
+                                        src={girl}
+                                    />
+                                    <Card.Body>
+                                        <Card.Title>Girl from Slytherin</Card.Title>
+                                        <Card.Text>Who is she?</Card.Text>
+                                        <Card.Text>Just a new student?</Card.Text>
+                                        <Card.Text>Perhaps.</Card.Text>
+                                        <Card.Text>She's frighteningly strange. She came here for a reason.</Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </div>
+
+                            <div className="col mt-4">
+                                <Card className="h-100" border="light">
+                                    <Card.Img
+                                        variant="top"
+                                        src={sortingHat}
+                                    />
+                                    <Card.Body>
+                                        <Card.Title>Sorting Hat</Card.Title>
+                                        Are you afraid of what you'll hear?
+                                        Afraid I'll speak the name you fear?
+                                        Not Slytherin! Not Gryffindor!
+                                        Not Hufflepuff! Not Ravenclaw!
+                                        Don't worry, child, I know my job,
+                                        You'll learn to laugh, if first you sob.
+                                    </Card.Body>
+                                </Card>
+                            </div>
+                        </div>
+                    </CardGroup>
+                </Container>
+            </>
         );
     }
 }
